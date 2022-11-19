03de8f2f9c907a7f0431e4f62971d66423c789ac
Penguatix <88264676+Penguatix@users.noreply.github.com>
2022-11-19 22:38:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,38 +9,30 @@ const client = new Client({
 		GatewayIntentBits.GuildMembers,
 	],
 });
+const fs = require('node:fs');
+const path = require('node:path');
 
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-client.login(token);
-
+// eslint-disable-next-line no-undef
 client.commands = new Collection();
-
-const commandsPath = path.join(__dirname, 'Commands/General');
+const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	}
-	else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+	client.commands.set(command.data.name, command);
 }
+
+client.once(Events.ClientReady, () => {
+	console.log('Ready!');
+});
+
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.commands.get(interaction.commandName);
+	const command = client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+	if (!command) return;
 
 	try {
 		await command.execute(interaction);
@@ -50,3 +42,5 @@ client.on(Events.InteractionCreate, async interaction => {
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
+
+client.login(token);
