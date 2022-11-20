2dca0515b33a590308887e19de5e4e7b3cb9430a
Penguatix <88264676+Penguatix@users.noreply.github.com>
2022-11-20 15:06:57 +0530

    Update ping.js

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,10 +1,16 @@
+const wait = require('node:timers/promises').setTimeout;
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Check The Ping <3'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.deferReply();
+		await wait(4000);
+		await interaction.editReply('Pong!');
+		const message = await interaction.fetchReply();
+		console.log(message);
+		await interaction.followUp('Pong Again');
 	},
 };
