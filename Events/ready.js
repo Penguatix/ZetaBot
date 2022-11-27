28ce4cefa02049e41a14a30d0a7775e1201c31b4
Supash <88264676+Penguatix@users.noreply.github.com>
2022-11-27 11:16:48 +0530

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -4,6 +4,7 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
+		client.user.setPresence({ activities: [{ name: 'Under Development' }], status: 'online' }),
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
